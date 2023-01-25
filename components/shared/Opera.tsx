f438b86e066d6ffc023fe30f823ca061330283e8
Paolo Missagia <login@paolomissagia.com>
2023-01-25 22:23:30 +0000

    create single opera component for display

---
+++ components/shared/Opera.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+export default function Example({ allOperas }: { allOperas: Array<object> }) {
+  return (
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {allOperas.map((opera: any) => (
+          <a key={opera.id} href={opera.href} className="group">
+            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-7">
+              <Image
+                src={opera.image}
+                alt=""
+                className="w-full h-full object-center object-cover group-hover:opacity-75"
+              />
+            </div>
+            <h3 className="mt-4 text-sm text-gray-700">{opera.composer}</h3>
+            <p className="mt-1 text-lg font-medium text-gray-900">
+              {opera.title}
+            </p>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
