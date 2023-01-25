f1a4a6fd7847a192c46742081c2d6d13271b6dd0
Paolo Missagia <login@paolomissagia.com>
2023-01-25 23:50:21 +0000

    updated component to use dynamic relative path for poster

--- components/shared/Opera.tsx
+++ components/shared/Opera.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 export default function Example({ allOperas }: { allOperas: Array<object> }) {
+  console.log(allOperas);
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
@@ -7,8 +8,10 @@ export default function Example({ allOperas }: { allOperas: Array<object> }) {
           <a key={opera.id} href={opera.href} className="group">
             <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-7">
               <Image
-                src={opera.image}
+                src={`/posters/${opera.image}`}
                 alt=""
+                width={500}
+                height={500}
                 className="w-full h-full object-center object-cover group-hover:opacity-75"
               />
             </div>
