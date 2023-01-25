6981cd86528bf17e1f2fcbbdac271555a07276cb
Paolo Missagia <login@paolomissagia.com>
2023-01-25 22:44:09 +0000

    updated api connection on backend

---
+++ pages/api/operas/index.ts
@@ -0,0 +1,34 @@
+import dbConnect from "../../../lib/dbConnect";
+import OperaModel from "@/models/OperaModel";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const pets = await OperaModel.find({});
+        res.status(200).json({ success: true, data: pets });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const pet = await OperaModel.create(req.body);
+        res.status(201).json({ success: true, data: pet });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
